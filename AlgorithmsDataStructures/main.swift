0427ba2982020f9b0760477f6f049bf14e9d9a59
Macbook Pro <gmjetomo@outlook.com>
2021-03-10 11:30:06 -0800

    initial commit

---
+++ AlgorithmsDataStructures/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  AlgorithmsDataStructures
+//
+//  Created by Gil Jetomo on 2021-03-09.
+//
+
+import Foundation
+
---
+++ Assignments/Recursion-Evaluate.js
@@ -0,0 +1,24 @@
+const evaluate = function(expression) {
+    if(expression.indexOf(")") === -1) {
+        return expression
+    }
+    
+    if((expression.split(")").length - 1) ===  1) {
+        return eval(expression)
+    }
+
+    str = expression.substr(
+        expression.lastIndexOf("("),
+        expression.indexOf(")"))
+    str = str.substr(0, str.indexOf(")")+1)
+
+    result = eval(str)
+    
+   return evaluate(expression.replace(str, result))
+
+}
+
+console.log(evaluate("7"))
+console.log(evaluate("(2+2)"))
+console.log(evaluate("(1+(2*4))"))
+console.log(evaluate("((1+3)+((1+2)*5))"))
