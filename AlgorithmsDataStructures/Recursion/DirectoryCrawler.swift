c3b986d94cac74c9cb4cf5c14c92a2b1fbe8f327
Macbook Pro <gmjetomo@outlook.com>
2021-03-13 10:45:02 -0800

    initial commit: mini project

--- AlgorithmsDataStructures/Recursion/DirectoryCrawler.swift
+++ AlgorithmsDataStructures/Recursion/DirectoryCrawler.swift
@@ -14,43 +14,40 @@ struct Symbol {
 }
 
 let fileManager = FileManager.default
+var fileCount = 0
+var directoryCount = 0
 
 func crawler() {
   let filePath = "/Users/macbookpro/Documents/Cornerstone/ConnectionToSQL"
   crawlerHelper(filePath: filePath, soFar: "")
+  print("Directories: \(directoryCount), Files: \(fileCount)")
 }
 
 func crawlerHelper(filePath: String, fileName: String = "", soFar: String, indent: String = "") {
-  guard let files = try? fileManager.contentsOfDirectory(atPath: filePath), files.count > 0 else {
+  guard let files = try? fileManager.contentsOfDirectory(atPath: filePath) else {
     let prevFilePath = String(filePath[...filePath.index(before: filePath.lastIndex(of: "/")!)])
     let files = try! fileManager.contentsOfDirectory(atPath: prevFilePath)
     var symbol = ""
-    if files.last == fileName {
-      (symbol = Symbol.lastFile)
-    } else {
-      (symbol = Symbol.mainDirectory)
-    }
+    symbol = files.last == fileName ? Symbol.lastFile : Symbol.mainDirectory
+    fileCount += 1
     print(soFar + indent + symbol + " " + fileName)
     return
   }
   
   for i in 0..<files.count {
+    var symbol = ""
     if i == 0 {
-      let file = String(filePath[filePath.index(after: filePath.lastIndex(of: "/")!)...])
-      let prevFilePath = String(filePath[...filePath.index(before: filePath.lastIndex(of: "/")!)])
-      //      var isDir : ObjCBool = false
-      var symbol = ""
-      //      if fileManager.fileExists(atPath: prevFilePath, isDirectory: &isDir) {
-      let files = try! fileManager.contentsOfDirectory(atPath: prevFilePath)
-      if files.last == file {
-        (symbol = Symbol.lastFile)
-      } else {
-        (symbol = Symbol.mainDirectory)
-      }
-      //      }
-      print(soFar + indent + symbol + " " + file)
+      let fileName = String(filePath[filePath.index(after: filePath.lastIndex(of: "/")!)...])
+      symbol = files.last == files[0] ? Symbol.lastFile : Symbol.mainDirectory
+      directoryCount += 1
+      print(soFar + indent + symbol + " " + fileName)
     }
+    symbol = Symbol.pipe
+    
     let file = files[i]
-    crawlerHelper(filePath: filePath + "/" + file, fileName: file, soFar: Symbol.pipe, indent: indent + "   ")
+    crawlerHelper(filePath: filePath + "/" + file,
+                  fileName: file,
+                  soFar: soFar + indent + symbol,
+                  indent: indent + " ")
   }
 }
