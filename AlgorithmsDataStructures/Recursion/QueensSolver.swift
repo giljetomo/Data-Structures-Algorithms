c88ab34b6d49e03b54f998e1563e78f444744af3
Macbook Pro <gmjetomo@outlook.com>
2021-03-15 23:01:15 -0700

    initial commit

--- AlgorithmsDataStructures/Recursion/QueensSolver.swift
+++ AlgorithmsDataStructures/Recursion/QueensSolver.swift
@@ -15,9 +15,30 @@ import Foundation
 /// - You are allowed to change the function header (args or return type)
 /// - Your total recursive calls should not exceed 120 times.
 
-//var count = 0
+var count = 0
 func solveQueens(board: inout Board) {
-//	count += 1
-	
+  	
+  var b = board
+  _ = solveQueensHelper(board: &b)
+  print(b)
+  print("Total recursive calls: \(count)")
 }
 
+func solveQueensHelper(board: inout Board, col: Int = 0) -> Bool {
+  count += 1
+  if col >= board.size {
+    return true
+  }
+  
+  for i in 0..<board.size {
+    if board.isSafe(row: i, col: col) {
+      board.place(row: i, col: col)
+      
+      if solveQueensHelper(board: &board, col: col + 1) {
+        return true
+      }
+      board.remove(row: i, col: col)
+    }
+  }
+  return false
+}
