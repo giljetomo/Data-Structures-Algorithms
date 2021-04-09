3563f74661ae47caae62d7a9642f887f2f7a8cc1
Macbook Pro <gmjetomo@outlook.com>
2021-04-09 11:06:25 -0700

    done

--- AlgorithmsDataStructures/GreedyAlgorithms/GasStation.swift
+++ AlgorithmsDataStructures/GreedyAlgorithms/GasStation.swift
@@ -10,23 +10,36 @@ import Foundation
 func canCompleteCircuit(_ gas: [Int], _ cost: [Int]) -> Int {
   
   var start = 0
-  var pivot = 0
   var tank = 0
+  var total = 0
   
-  while true {
-    tank += gas[pivot]
-    tank -= cost[pivot]
-    
-    if tank >= 0 {
-      pivot = gas.count - 1 == pivot ? 0 : pivot + 1
-      if pivot == start { return start }
-    } else {
-      start = gas.count - 1 == start ? -1 : start + 1
-      if start == -1 { return -1 }
-      else {
-        tank = 0
-        pivot = start
-      }
+  // O(n)
+  for i in 0..<gas.count {
+    tank += gas[i] - cost[i]
+    if tank < 0 {
+      start = i + 1
+      tank = 0
     }
+    total += gas[i] - cost[i]
   }
+  
+  return total >= 0 ? start : -1
+  
+  ///alternative implementation:  O(n^2)
+  //  var pivot = 0
+  //  while true {
+  //    tank += gas[pivot] - cost[pivot]
+  //
+  //    if tank >= 0 {
+  //      pivot = gas.count - 1 == pivot ? 0 : pivot + 1
+  //      if pivot == start { return start }
+  //    } else {
+  //      start = gas.count - 1 == start ? -1 : start + 1
+  //      if start == -1 { return -1 }
+  //      else {
+  //        tank = 0
+  //        pivot = start
+  //      }
+  //    }
+  //  }
 }
